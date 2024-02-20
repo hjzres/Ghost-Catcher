1ab9a2a64dfce2aa20d28831837cb064e2c9d3d9
Andrei <142443853+hjzres@users.noreply.github.com>
2024-02-20 02:06:40 -0500

    Stamina System Working, but even if he isn't moving it can go down if he is holding shift

--- Assets/Scripts/Player/PlayerCam.cs
+++ Assets/Scripts/Player/PlayerCam.cs
@@ -1,31 +1,32 @@
 using UnityEngine;
 
-namespace player{
-    public class PlayerCam : MonoBehaviour
-    {
-        [SerializeField] private float sensitivity;
+namespace player
+{
+	public class PlayerCam : MonoBehaviour
+	{
+		[SerializeField] private float sensitivity;
 
-        [SerializeField] private Transform orentation;
+		[SerializeField] private Transform orentation;
 
-        private float xRotation;
-        private float yRotation;
+		private float xRotation;
+		private float yRotation;
 
-        private void Awake(){
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
+		private void Awake(){
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
 
-        private void Update(){
-            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivity; 
-            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivity; 
+		private void Update(){
+			float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivity; 
+			float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivity; 
 
-            yRotation += mouseX;
-            xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -90, 90);
+			yRotation += mouseX;
+			xRotation -= mouseY;
+			xRotation = Mathf.Clamp(xRotation, -90, 90);
 
-            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-            orentation.rotation = Quaternion.Euler(0, yRotation, 0);
+			transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+			orentation.rotation = Quaternion.Euler(0, yRotation, 0);
 
-        }
-    }
+		}
+	}
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,101 +1,144 @@
 using UnityEngine;
 
-namespace player{
-    public class PlayerMovement : MonoBehaviour
-    {
-        [Header("Movement")]
-        [SerializeField] [ReadOnly] private float moveSpeed;
-        [SerializeField] private float walkSpeed;
-        [SerializeField] private float runSpeed;
+namespace player
+{
+	public class PlayerMovement : MonoBehaviour
+	{
+		[Header("Movement")]
+		[SerializeField] [ReadOnly] private float moveSpeed;
+		[SerializeField] private float walkSpeed;
+		[SerializeField] private float runSpeed;
+		public float RunSpeed{
+			get
+			{
+				UseStamina();
+				return runSpeed;
+			}
+			set
+			{
+				runSpeed = value;
+			}
+		}
+		[SerializeField] private float groundDrag;
 
-        [SerializeField] private float groundDrag;
-        [SerializeField] private float jumpForce;
-        [SerializeField] private float jumpCooldown;
-        [SerializeField] private float airMultiplier;
-        private bool _readyToJump;
+		[Header("Stamina")]
+		[SerializeField] [Min(0)] private float maxStamina;
+		[SerializeField] [ReadOnly] private float stamina;
+		private float _staminaTime;
 
-        [Header("Keybinds")]
-        [SerializeField] private KeyCode jumpKey = KeyCode.Space;
-        [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+		[Header("Jump")]
+		[SerializeField] private float jumpForce;
+		[SerializeField] private float jumpCooldown;
+		[SerializeField] private float airMultiplier;
+		private bool _readyToJump;
+
+		[Header("Keybinds")]
+		[SerializeField] private KeyCode jumpKey = KeyCode.Space;
+		[SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
 
 
-        [Header("Ground Check")]
-        [SerializeField] private float playerHeight;
-        [SerializeField] private LayerMask whatisGround;
-        private bool _isGrounded;
+		[Header("Ground Check")]
+		[SerializeField] private float playerHeight;
+		[SerializeField] private LayerMask whatisGround;
+		private bool _isGrounded;
 
-        [SerializeField] private Transform orientation;
+		[SerializeField] private Transform orientation;
 
-        private float _horizontalInput;
-        private float _verticalInput;
+		private float _horizontalInput;
+		private float _verticalInput;
 
-        private Vector3 _moveDirection;
+		private Vector3 _moveDirection;
 
-        private Rigidbody _rb;
+		private Rigidbody _rb;
 
-        private void Start() {
-            _rb = GetComponent<Rigidbody>();
-            _rb.freezeRotation = true;
-            _readyToJump = true;
-        }
+		private void Start() 
+		{
+			_rb = GetComponent<Rigidbody>();
+			_rb.freezeRotation = true;
+			_readyToJump = true;
+			stamina = maxStamina;
+		}
 
-        private void Update() {
-            _isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatisGround);
+		private void Update() 
+		{
+			_isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatisGround);
 
-            MyInput();
-            SpeedControl();
+			MyInput();
+			SpeedControl();
+			RegenStamina();
 
-            if(_isGrounded)
-                _rb.drag = groundDrag;
-            else 
-                _rb.drag = 0;
-        } 
+			if(_isGrounded)
+				_rb.drag = groundDrag;
+			else 
+				_rb.drag = 0;
+		} 
 
-        private void FixedUpdate() {
-            MovePlayer();
-        }
+		private void FixedUpdate() 
+		{
+			MovePlayer();
+		}
 
-        private void MyInput(){
-            _horizontalInput = Input.GetAxisRaw("Horizontal");
-            _verticalInput = Input.GetAxisRaw("Vertical");
+		private void MyInput()
+		{
+			_horizontalInput = Input.GetAxisRaw("Horizontal");
+			_verticalInput = Input.GetAxisRaw("Vertical");
 
-            if(Input.GetKey(jumpKey) && _readyToJump && _isGrounded){
-                _readyToJump = false;
+			if(Input.GetKey(jumpKey) && _readyToJump && _isGrounded)
+			{
+				_readyToJump = false;
 
-                Jump();
+				Jump();
 
-                Invoke(nameof(ResetJump), jumpCooldown);
-            }
-        }
+				Invoke(nameof(ResetJump), jumpCooldown);
+			}
+		}
 
-        private void MovePlayer(){
-            _moveDirection = orientation.forward * _verticalInput + orientation.right * _horizontalInput;
-            moveSpeed = Input.GetKey(sprintKey) ? runSpeed : walkSpeed;
+		private void MovePlayer()
+		{
+			_moveDirection = orientation.forward * _verticalInput + orientation.right * _horizontalInput;
+			moveSpeed = Input.GetKey(sprintKey) && stamina > 0 ? RunSpeed : walkSpeed;
 
-            if(_isGrounded)
-                _rb.AddForce(_moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-            else if(!_isGrounded)
-                _rb.AddForce(_moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
-        }
+			if(_isGrounded)
+				_rb.AddForce(_moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+			else if(!_isGrounded)
+				_rb.AddForce(_moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+		}
 
-        private void SpeedControl(){
-            Vector3 flatVel = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
+		private void SpeedControl(){
+			Vector3 flatVel = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
 
-            if(flatVel.magnitude > moveSpeed){
-                Vector3 limitedVel = flatVel.normalized * moveSpeed;
-                _rb.velocity = new Vector3(limitedVel.x, _rb.velocity.y, limitedVel.z);
-            }
-        }
+			if(flatVel.magnitude > moveSpeed)
+			{
+				Vector3 limitedVel = flatVel.normalized * moveSpeed;
+				_rb.velocity = new Vector3(limitedVel.x, _rb.velocity.y, limitedVel.z);
+			}
+		}
 
-        private void Jump(){
-            _rb.velocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
+		private void Jump(){
+			_rb.velocity = new Vector3(_rb.velocity.x, 0f, _rb.velocity.z);
 
-            _rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+			_rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
 
-        }
+		}
 
-        private void ResetJump(){
-            _readyToJump = true;
-        }
-    }
+		private void ResetJump(){
+			_readyToJump = true;
+		}
+
+		private void RegenStamina(){
+			if(_staminaTime < 2f)
+			{
+				_staminaTime += Time.deltaTime;
+			}
+			else if(stamina < maxStamina)
+			{
+				stamina += 10 * Time.deltaTime;
+			}
+		}
+
+		private void UseStamina(){
+			_staminaTime = 0;
+			stamina -= 20 * Time.deltaTime;
+		}
+	}
 }
