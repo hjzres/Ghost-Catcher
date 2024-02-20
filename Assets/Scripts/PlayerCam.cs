45ef0ee87b07cf1df543845d06ddabfb7eedfe41
Andrei <142443853+hjzres@users.noreply.github.com>
2024-02-19 22:45:28 -0500

    Done Player Movement

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerCam : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float sensitivity;
+
+    [SerializeField] private Transform orentation;
+
+    private float xRotation;
+    private float yRotation;
+
+    private void Awake(){
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update(){
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivity; 
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivity; 
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orentation.rotation = Quaternion.Euler(0, yRotation, 0);
+
     }
 }
