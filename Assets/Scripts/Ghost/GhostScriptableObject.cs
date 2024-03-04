cbb828bfd0187e751da30887eef9331930eedb39
Andrei <142443853+hjzres@users.noreply.github.com>
2024-03-04 09:05:47 -0500

    Started Ghost Scriptable Object

---
+++ Assets/Scripts/Ghost/GhostScriptableObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewGhost", menuName = "Ghost")]
+public class GhostScriptableObject : ScriptableObject
+{
+	
+}
